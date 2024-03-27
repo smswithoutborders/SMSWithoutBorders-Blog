dc55bd5b369e6160b15e54d817fbde4544a8cb03
Mildred <99267408+mildrette@users.noreply.github.com>
2024-03-27 22:29:45 +0100

    remove link to next js favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
 				<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
 				<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
 				<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-				<link rel="manifest" href="/favicon/site.webmanifest" />
 				<link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
 				<link rel="shortcut icon" href="/favicon/favicon.ico" />
 				<meta name="msapplication-TileColor" content="#000000" />
