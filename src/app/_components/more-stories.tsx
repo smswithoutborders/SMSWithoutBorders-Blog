f536cf4179e30fc2d0ed8a3c6393ed9a6a5cb5c6
Mildred <99267408+mildrette@users.noreply.github.com>
2024-03-28 11:09:22 +0100

    fix: Update unlinted files

--- src/app/_components/more-stories.tsx
+++ src/app/_components/more-stories.tsx
@@ -6,21 +6,21 @@ type Props = {
 };
 
 export function MoreStories({ posts }: Props) {
-  return (
-    <section className="my-5 gap-y-5 pt-20">
-      <div className="my-5 md:w-2/4 mx-auto  lg:gap-x-35  mb-52">
-        {posts.map((post) => (
-          <PostPreview
-            className="my-5 gap-y-5 p-5 mb-32"
-            key={post.slug}
-            title={post.title}
-            date={post.date}
-            author={post.author}
-            slug={post.slug}
-            excerpt={post.excerpt}
-          />
-        ))}
-      </div>
-    </section>
-  );
+	return (
+		<section className="my-5 gap-y-5 pt-20">
+			<div className="my-5 md:w-2/4 mx-auto  lg:gap-x-35  mb-52">
+				{posts.map((post) => (
+					<PostPreview
+						className="my-5 gap-y-5 p-5 mb-32"
+						key={post.slug}
+						title={post.title}
+						date={post.date}
+						author={post.author}
+						slug={post.slug}
+						excerpt={post.excerpt}
+					/>
+				))}
+			</div>
+		</section>
+	);
 }
