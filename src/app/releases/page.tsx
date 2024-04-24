86269444ac001ed9a8025bdfa1d8fd4ffea8ca8f
mildrette <mildredfonka1@gmail.com>
2024-04-24 16:17:10 +0100

    change smswithoutborders to swob

--- src/app/releases/page.tsx
+++ src/app/releases/page.tsx
@@ -9,8 +9,8 @@ export default function Index() {
 		<main>
 			<Container>
 				{allReleases.length === 0 ? (
-					<div className="py-60 px-8 mt-10">
-						<div className="mx-auto max-w-lg py-10 px-8 bg-white shadow-md rounded-lg">
+					<div className="py-20 md:py-60 px-4 md:px-8 mt-10 md:mt-0">
+						<div className="mx-auto max-w-[90%] md:max-w-lg py-10 px-8 bg-white shadow-md rounded-lg">
 							<p className="text-xl text-center font-bold text-gray-700">
 								No release posts available yet.
 							</p>
